0b597e756484c03ef5d7b3a311bb3c72bf7a1c22
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-01-29 00:30:24 -0400

    Index, imagenes y presentacion pizza

--- ProyectoPizzaPlace/Models/CanastaCompras.cs
+++ ProyectoPizzaPlace/Models/CanastaCompras.cs
@@ -5,4 +5,5 @@ public class CanastaCompras
     public Clientes Cliente { get; set; } = new Clientes();
     public List<int> Pedidos { get; set; } = new List<int>();
     public bool Pago { get; set; }
+    public void Add(int pizzaId) => Pedidos.Add(pizzaId);
 }
